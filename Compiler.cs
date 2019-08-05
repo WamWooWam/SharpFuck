4e87f82bf61cc31c0d2efb4321b8a53eb8c4bb31
Thomas May <wamwoowam@gmail.com>
2019-08-05 18:07:59 +0100

    Commit Code

---
+++ Compiler.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpFuck
+{
+    public struct Token
+    {
+        public byte token;
+        public int count;
+    }
+
+    public class Compiler
+    {
+        private struct Loop
+        {
+            public Label start;
+            public Label condition;
+        }
+
+        private AssemblyBuilder _asm;
+        private Type _type;
+        private MethodInfo _method;
+        private Stack<int> _stack = new Stack<int>();
+
+        private static readonly byte[] _validTokens = new[]
+        {
+            (byte)'>',
+            (byte)'<',
+            (byte)'+',
+            (byte)'-',
+            (byte)'.',
+            (byte)',',
+            (byte)'[',
+            (byte)']'
+        };
+
+        private static readonly MethodInfo _writeConsoleMethod 
+            = typeof(Console).GetMethod("Write", new[] { typeof(char) });
+        private static readonly MethodInfo _readKeyMethod 
+            = typeof(Console).GetMethod("ReadKey", Type.EmptyTypes);
+        private static readonly MethodInfo _getKeyCodeMethod 
+            = typeof(ConsoleKeyInfo).GetProperty("KeyChar").GetMethod;
+
+        public Compiler()
+        {
+            AssemblyName = "BF2IL";
+            TypeName = "BF2IL";
+            MethodName = "Execute";
+        }
+
+        public string AssemblyName { get; set; }
+        public string OutputFileName { get; set; }
+        public string TypeName { get; set; }
+        public string MethodName { get; set; }
+        public bool SkipIO { get; set; }
+
+        /// <summary>
+        /// Processes a list of <see cref="Token"/>s from a stream
+        /// </summary>
+        /// <param name="stream">The stream to read tokens from</param>
+        /// <returns>A list of <see cref="Token"/>s</returns>
+        public IReadOnlyList<Token> Tokenise(Stream stream)
+        {
+            var i = 0;
+            var tokens = new List<Token>();
+            var currentToken = new Token();
+            while ((i = stream.ReadByte()) != -1)
+            {
+                var b = (byte)i;
+
+                if (currentToken.token == b)
+                {
+                    currentToken.count++;
+                }
+                else
+                {
+                    if (currentToken.token != 0 && currentToken.count != 0)
+                        tokens.Add(currentToken);
+
+                    currentToken = new Token() { count = 1, token = b };
+                }
+            }
+
+            tokens.RemoveAll(t => !_validTokens.Contains(t.token));
+
+            return tokens;
+        }
+
+        /// <summary>
+        /// Compiles a list of <see cref="Token"/>s to IL.
+        /// </summary>
+        /// <param name="tokens"></param>
+        public void CompileTokens(IReadOnlyList<Token> tokens)
+        {
+            var labels = new Stack<Loop>();
+            _asm = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(AssemblyName), AssemblyBuilderAccess.RunAndSave);
+
+            var mod = _asm.DefineDynamicModule(AssemblyName, OutputFileName ?? AssemblyName + ".exe");
+            
+            var type = mod.DefineType(TypeName, TypeAttributes.Public);
+            var method = type.DefineMethod(MethodName, MethodAttributes.Public | MethodAttributes.Static);
+            var gen = method.GetILGenerator();
+
+            gen.DeclareLocal(typeof(int)); // 0
+            gen.DeclareLocal(typeof(byte[])); // 1
+            var key_field = gen.DeclareLocal(typeof(ConsoleKeyInfo)); // 2
+
+            // init the ptr field to 0
+            gen.Emit(OpCodes.Ldc_I4_0);
+            gen.Emit(OpCodes.Stloc_0);
+
+            // create the mem array
+            gen.Emit(OpCodes.Ldc_I4, 262144);
+            gen.Emit(OpCodes.Newarr, typeof(byte));
+            gen.Emit(OpCodes.Stloc_1);
+
+            foreach (var token in tokens)
+            {
+                switch (token.token)
+                {
+                    case (byte)'>':
+                        CompileIncrementPointerToken(gen, token);
+                        break;
+                    case (byte)'<':
+                        CompileDecrementPointerToken(gen, token);
+                        break;
+                    case (byte)'+':
+                        CompileIncrementValueToken(gen, token);
+                        break;
+                    case (byte)'-':
+                        CompileDecrementValueToken(gen, token);
+                        break;
+                    default:
+                        for (var i = 0; i < token.count; i++)
+                        {
+                            switch (token.token)
+                            {
+                                case (byte)'.':
+                                    if (!SkipIO) CompilePutCharToken(gen);
+                                    break;
+                                case (byte)',':
+                                    if (!SkipIO) CompileGetCharToken(gen, key_field);
+                                    break;
+                                case (byte)'[':
+                                    CompileLoopStartToken(labels, gen);
+                                    break;
+                                case (byte)']':
+                                    CompileLoopEndToken(labels, gen);
+                                    break;
+                                default:
+                                    throw new InvalidOperationException("Invalid token encountered!");
+                            }
+                        }
+                        break;
+                }
+            }
+
+            gen.Emit(OpCodes.Ret);
+
+            _type = type.CreateType();
+            _method = _type.GetMethod(MethodName);
+        }
+
+        /// <summary>
+        /// Executes the compiled output from <see cref="CompileTokens(IReadOnlyList{Token})"/>
+        /// </summary>
+        public void Execute()
+        {
+            if (_asm == null)
+                throw new InvalidOperationException("Nothing has been compiled yet!");
+
+            _method.Invoke(null, null);
+        }
+
+        /// <summary>
+        /// Saves the compiled output from <see cref="CompileTokens(IReadOnlyList{Token})"/> to disk as a class library.
+        /// </summary>
+        public void SaveAsClassLibrary()
+        {
+            if (_asm == null)
+                throw new InvalidOperationException("Nothing has been compiled yet!");
+
+            _asm.Save(OutputFileName);
+        }
+
+        /// <summary>
+        /// Saves the compiled output from <see cref="CompileTokens(IReadOnlyList{Token})"/> to disk as an executable.
+        /// </summary>
+        public void SaveAsExecutable()
+        {
+            if (_asm == null)
+                throw new InvalidOperationException("Nothing has been compiled yet!");
+
+            _asm.SetEntryPoint(_method);
+            _asm.Save(OutputFileName);
+        }
+
+        /// <summary>
+        /// Compiles a group of > tokens
+        /// </summary>
+        private void CompileIncrementPointerToken(ILGenerator gen, Token token)
+        {
+            gen.Emit(OpCodes.Ldloc_0);
+            gen.Emit(OpCodes.Ldc_I4, token.count);
+            gen.Emit(OpCodes.Add);
+            gen.Emit(OpCodes.Stloc_0);
+        }
+
+        /// <summary>
+        /// Compiles a group of < tokens
+        /// </summary>
+        private void CompileDecrementPointerToken(ILGenerator gen, Token token)
+        {
+            gen.Emit(OpCodes.Ldloc_0);
+            gen.Emit(OpCodes.Ldc_I4, token.count);
+            gen.Emit(OpCodes.Sub);
+            gen.Emit(OpCodes.Stloc_0);
+        }
+
+        /// <summary>
+        /// Compiles a group of + tokens
+        /// </summary>
+        private void CompileIncrementValueToken(ILGenerator gen, Token token)
+        {
+            gen.Emit(OpCodes.Ldloc_1);
+            gen.Emit(OpCodes.Ldloc_0);
+            gen.Emit(OpCodes.Ldloc_1);
+            gen.Emit(OpCodes.Ldloc_0);
+            gen.Emit(OpCodes.Ldelem_U1);
+            gen.Emit(OpCodes.Ldc_I4, token.count);
+            gen.Emit(OpCodes.Add);
+            gen.Emit(OpCodes.Conv_U1);
+            gen.Emit(OpCodes.Stelem_I1);
+        }
+
+        /// <summary>
+        /// Compiles a group of - tokens
+        /// </summary>
+        private void CompileDecrementValueToken(ILGenerator gen, Token token)
+        {
+            gen.Emit(OpCodes.Ldloc_1);
+            gen.Emit(OpCodes.Ldloc_0);
+            gen.Emit(OpCodes.Ldloc_1);
+            gen.Emit(OpCodes.Ldloc_0);
+            gen.Emit(OpCodes.Ldelem_U1);
+            gen.Emit(OpCodes.Ldc_I4, token.count);
+            gen.Emit(OpCodes.Sub);
+            gen.Emit(OpCodes.Conv_U1);
+            gen.Emit(OpCodes.Stelem_I1);
+        }
+
+        /// <summary>
+        /// Compiles a . token
+        /// </summary>
+        private void CompilePutCharToken(ILGenerator gen)
+        {
+            gen.Emit(OpCodes.Ldloc_1);
+            gen.Emit(OpCodes.Ldloc_0);
+            gen.Emit(OpCodes.Ldelem_U1);            
+            gen.Emit(OpCodes.Call, _writeConsoleMethod);
+        }
+
+        /// <summary>
+        /// Compiles a , token
+        /// </summary>
+        private void CompileGetCharToken(ILGenerator gen, LocalBuilder key_field)
+        {
+            gen.Emit(OpCodes.Ldloc_1);
+            gen.Emit(OpCodes.Ldloc_0);
+            gen.Emit(OpCodes.Call, _readKeyMethod);
+            gen.Emit(OpCodes.Stloc_2);
+            gen.Emit(OpCodes.Ldloca, key_field);
+            gen.Emit(OpCodes.Call, _getKeyCodeMethod);
+            gen.Emit(OpCodes.Conv_U1);
+            gen.Emit(OpCodes.Stelem_I1);
+        }
+
+        /// <summary>
+        /// Compiles a [ token
+        /// </summary>
+        private void CompileLoopStartToken(Stack<Loop> labels, ILGenerator gen)
+        {
+            var loop = new Loop
+            {
+                condition = gen.DefineLabel(),
+                start = gen.DefineLabel()
+            };
+
+            gen.Emit(OpCodes.Br, loop.condition);
+            gen.MarkLabel(loop.start);
+            labels.Push(loop);
+        }
+
+        /// <summary>
+        /// Compiles a ] token
+        /// </summary>
+        private void CompileLoopEndToken(Stack<Loop> labels, ILGenerator gen)
+        {
+            var loop = labels.Pop();
+            gen.MarkLabel(loop.condition);
+
+            gen.Emit(OpCodes.Ldloc_1);
+            gen.Emit(OpCodes.Ldloc_0);
+            gen.Emit(OpCodes.Ldelem_U1);
+            gen.Emit(OpCodes.Brtrue, loop.start);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace SharpFuck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parsed_args = new Dictionary<string, string>();
+            foreach (var str in args)
+            {
+                if (!string.IsNullOrWhiteSpace(str) && str.Contains('-'))
+                {
+                    var arg = str.TrimStart('-');
+                    var end = arg.IndexOf("=");
+                    if (end == -1)
+                    {
+                        parsed_args.Add(arg, null);
+                    }
+                    else
+                    {
+                        parsed_args.Add(arg.Substring(0, arg.IndexOf('=')), arg.Substring(arg.IndexOf('=') + 1));
+                    }
+                }
+            }
+
+            if (parsed_args.ContainsKey("help"))
+            {
+                Console.WriteLine("SharpFuck - BrainFuck -> IL Compiler");
+                Console.WriteLine();
+                Console.WriteLine("Usage: sharpfuck.exe");
+                Console.WriteLine("    --help");
+                Console.WriteLine("        Show this help text.");
+                Console.WriteLine();
+                Console.WriteLine("    --run=<filename>");
+                Console.WriteLine("        Runs a brainfuck file by compiling then executing it.");
+                Console.WriteLine("    --bench");
+                Console.WriteLine("        Measures the amount of time required to run the specified file.");
+                Console.WriteLine();
+                Console.WriteLine("    --compile=<filename>");
+                Console.WriteLine("        Compiles a brainfuck file.");
+                Console.WriteLine("    --out=<filename>");
+                Console.WriteLine("        Specifies the output of the compiler. Defaults to <in_filename> + \".exe\".");
+                Console.WriteLine("        If you wish to output a class library, make sure the file name ends in \".dll\".");
+                Console.WriteLine();
+                Console.WriteLine("    --assembly=<name>");
+                Console.WriteLine("        Specifies the resulting assembly name. Defaults to \"BF2IL\".");
+                Console.WriteLine("    --type=<name>");
+                Console.WriteLine("        Specifies the resulting type name. Can contain namespaces. Defaults to \"BF2IL\".");
+                Console.WriteLine("    --method=<name>");
+                Console.WriteLine("        Specifies the resulting method name. Defaults to \"Execute\".");
+                Console.WriteLine();
+
+                return;
+            }
+
+            var compiler = new Compiler();
+
+            if (parsed_args.TryGetValue("out", out var o))
+            {
+                compiler.OutputFileName = o;
+            }
+
+            if (parsed_args.TryGetValue("assembly", out var a))
+            {
+                compiler.AssemblyName = a;
+            }
+
+            if (parsed_args.TryGetValue("type", out var t))
+            {
+                compiler.TypeName = t;
+            }
+
+            if (parsed_args.TryGetValue("method", out var m))
+            {
+                compiler.MethodName = m;
+            }
+
+            if (parsed_args.ContainsKey("skip_io"))
+            {
+                compiler.SkipIO = true;
+            }
+
+            if (parsed_args.TryGetValue("compile", out var file))
+            {
+                using (var stream = File.OpenRead(file))
+                {
+                    if (compiler.OutputFileName == null)
+                    {
+                        compiler.OutputFileName = Path.GetFileNameWithoutExtension(file) + ".exe";
+                    }
+
+                    Console.WriteLine($"Compiling {Path.GetFileName(file)}...");
+                    var tokens = compiler.Tokenise(stream);
+
+                    Console.WriteLine($"Compiling {tokens.Count} tokens...");
+
+                    compiler.CompileTokens(tokens);
+                    Console.WriteLine($"Compiled {tokens.Count} tokens!");
+
+                    if (Path.GetExtension(compiler.OutputFileName) == ".exe")
+                    {
+                        compiler.SaveAsExecutable();
+                    }
+                    else
+                    {
+                        compiler.SaveAsClassLibrary();
+                    }
+
+                    Console.WriteLine($"Saved to {compiler.OutputFileName}!");
+                }
+
+                return;
+            }
+
+            if (parsed_args.TryGetValue("run", out var file1))
+            {
+                using (var stream = File.OpenRead(file1))
+                {
+                    compiler.OutputFileName = "BF2IL.exe";
+
+                    var tokens = compiler.Tokenise(stream);
+                    compiler.CompileTokens(tokens);
+
+                    Stopwatch watch = null;
+                    if (parsed_args.ContainsKey("bench"))
+                    {
+                        watch = Stopwatch.StartNew();
+                    }
+
+                    compiler.Execute();
+
+                    if(watch != null)
+                    {
+                        Console.WriteLine(watch.Elapsed);
+                    }
+                }
+
+                return;
+            }
+
+            Console.WriteLine("No input specified. Use \"sharpfuck.exe --help\" for help!");
+
+        }
+    }
+}
